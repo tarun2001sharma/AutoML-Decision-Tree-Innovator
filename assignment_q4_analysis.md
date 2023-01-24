deecaf406ee070596a84c779bbe6b06c95469bf6
Tarun Sharma <tarun2365@gmail.com>
2023-01-24 18:26:05 +0530

    added q4

--- assignment_q4_analysis.md
+++ assignment_q4_analysis.md
@@ -1,11 +1,9 @@
 
-##### Learning by varying M
+##### Learning Tree and Prediction by varying M
 
 <p align="center">
   <img width="400" src="learn_M.png">
 </p>
-
-##### Prediction by varying M
 
 <p align="center">
   <img width="400" src="predict_M.png">
@@ -13,15 +11,15 @@
 
 1. Theoretical train time complexity of O(M) when number of examples is kept constant.
 2. In the learning graph shown above, the time taken increases with M but not linearly.
-3. Prediction Graph
+3. Test time complexity would be O(depth) since we have to move from root to a leaf node of the decision tree.
+
+----------------------------------------------
  
-##### Learning tree by varying N
+##### Learning tree and Prediction by varying N
 
 <p align="center">
   <img width="400" src="learn_N.png">
 </p>
-
-##### Prediction by varying N
 
 <p align="center">
   <img width="400" src="predict_N.png">
@@ -29,4 +27,4 @@
 
 1. Theoretical train time complexity of O( N * logN ) when number of attributes is kept constant.
 2. In the learning graph shown above, the time taken increases with N.
-3. Prediction Graph
+3. Test time complexity would be O(depth) since we have to move from root to a leaf node of the decision tree.
--- auto-efficiency.py
+++ auto-efficiency.py
@@ -8,57 +8,60 @@ from sklearn import tree
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+import warnings
+warnings.filterwarnings("ignore")
 
 np.random.seed(42)
 
 # Read real-estate data set
-
 attr = ["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration", "model year", "origin", "car name"]
-
 data = pd.read_fwf('auto-mpg.data', names=attr)
-print(data)
+
+# drop 'car name' column and remove missing values
 data.drop('car name', axis=1, inplace=True)
 data = data[data.horsepower != '?']
 data.reset_index(drop=True, inplace=True)
 
+# convert all columns to float64 data type
 for i in data.columns:
     data[i] = data[i].astype('float64')
 
-# data.drop(data.index[data['horsepower'] == "?"], inplace=True)
-# print(data)
-
+# split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,1:], data.iloc[:,0], test_size=0.2, random_state=0)
-My_Tree = DecisionTree(criterion='information_gain', max_depth=8)  # Split based on Inf. Gain
-My_Tree.fit(X_train, y_train)
-y_hat = My_Tree.predict(X_test)
+Tree_model = DecisionTree(criterion='information_gain', max_depth=8)  # Split based on Information Gain
+
+# fit the tree to the training data
+Tree_model.fit(X_train, y_train)
+y_hat = Tree_model.predict(X_test)
 y1=np.array(y_hat)-np.array(y_test)
 npY_hat = np.array(y_hat)
 npY_hat = [ '%.1f' % elem for elem in npY_hat ]
 npy_test = np.array(y_test)
+
+# print the comparison of original output vs My Tree Prediction
 print("Original Output vs My Tree Prediction")
 for i in range(len(npY_hat)):
     print(npy_test[i],"          ",npY_hat[i])
 
-My_Tree.plot()
+# plot the decision tree
+Tree_model.plot()
+print('Root Mean Squared error: ', rmse(y_hat, y_test))
+print('Mean Absolute Error: ', mae(y_hat, y_test))
 
-print('RMSE: ', rmse(y_hat, y_test))
-print('MAE: ', mae(y_hat, y_test))
+# create another decision tree using DecisionTreeRegressor from sklearn
+Sklearn_DecisionTree = DecisionTreeRegressor(max_depth=8)
+Sklearn_DecisionTree.fit(X_train, y_train)      # fit the tree to the training data
 
+Plot_Sk = tree.export_text(Sklearn_DecisionTree)
+y_hat_new = Sklearn_DecisionTree.predict(X_test)
+y_hat_new = pd.Series(y_hat_new)
+y2 = np.array(y_hat_new)-np.array(y_hat)
 
-Sk_Tree = DecisionTreeRegressor(max_depth=8)
-Sk_Tree.fit(X_train, y_train)
-
-
-Plot_Sk = tree.export_text(Sk_Tree)
-y_hat_Sk = Sk_Tree.predict(X_test)
-y_hat_Sk = pd.Series(y_hat_Sk)
-y2 = np.array(y_hat_Sk)-np.array(y_hat)
-
-npyy_hat = np.array(y_hat_Sk)
+npyy_hat = np.array(y_hat_new)
 print("My Predicted Output vs SKlearn Predicted Output")
 for i in range(len(npY_hat)):
     print(npY_hat[i],"          ",npyy_hat[i])
 
-print('RMSE: ', rmse(y_hat_Sk, y_test))
-print('MAE: ', mae(y_hat_Sk, y_test))
+print('Root Mean Squared error: ', rmse(y_hat_new, y_test))
+print('Mean Absolute Error: ', mae(y_hat_new, y_test))
 
