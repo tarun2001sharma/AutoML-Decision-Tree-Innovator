41238a09e8844dc51c933a801b5ff64ae9c41052
Tarun Sharma <tarun2365@gmail.com>
2023-01-24 10:58:53 +0530

    added q2

--- auto-efficiency.py
+++ auto-efficiency.py
@@ -8,5 +8,14 @@ from metrics import *
 np.random.seed(42)
 
 # Read real-estate data set
-# ...
-# 
+
+attr = ["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration", "model year", "origin", "car name"]
+
+data = pd.read_fwf('auto-mpg.data', names=attr)
+print(data)
+data.drop('car name', axis=1, inplace=True)
+data = data[data.horsepower != '?']
+# data.drop(data.index[data['horsepower'] == "?"], inplace=True)
+print(data)
+
+
